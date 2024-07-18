9a1f78aca0395343051cabee856d816d146b5aab
youssofOkiel <youssofokiel3@gmail.com>
2024-07-19 00:18:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const PORT = 4000;
+const app = express();
+app.get('/', (req, res) => res.send('<h1>Hi Youssof !</h2>'))
+
+app.listen(PORT, () => console.log(`port is ${PORT}`));
