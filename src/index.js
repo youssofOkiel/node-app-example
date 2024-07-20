be35bfbf49099d3af24951cdddb20fb9e86b6d0d
youssofOkiel <youssofokiel3@gmail.com>
2024-07-20 07:20:18 +0300

    docker volumes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const PORT = 4000;
 const app = express();
-app.get('/', (req, res) => res.send('<h1>Hi Youssof test test !</h2>'))
+app.get('/', (req, res) => res.send('<h1>Hi Youssof test !</h2>'))
 
 app.listen(PORT, () => console.log(`port is ${PORT}`));
