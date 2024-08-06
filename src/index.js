2e77135c36245025a041316634446864fb4d525d
youssofOkiel <youssofokiel3@gmail.com>
2024-08-06 22:59:38 +0300

    fix db credentials

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const app = express();
 
 
 const DB_USER = 'root';
-const DB_PASSWORD = 'passowrd';
-const DB_HOST = '172.19.0.3';
+const DB_PASSWORD = 'password';
+const DB_HOST = 'mongo';
 const DB_PORT = 27017;
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 
